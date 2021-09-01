de7de6ab907fde522fc0bef11775c942ec224c83
Sylvain Gournay <sylvain.gournay@prismea.fr>
2021-09-01 17:47:35 +0200

    Initial Spring commit

---
+++ src/main/java/fr/sgo/mastermindserver/MastermindServerApplication.java
@@ -0,0 +1,13 @@
+package fr.sgo.mastermindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sgo/mastermindserver/MastermindServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sgo.mastermindserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
